72856a7cd221b7715ca7ec0519e882a2a61d59cf
kb12233 <kaybeeocfemia@gmail.com>
2024-07-14 20:42:53 +0800

    moved assets to public folder

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Profile() {
   // Function to open the resume PDF
   const openResume = () => {
-    window.open('ocfemia_portfolio_react/src/assets/Ocfemia_Resume.pdf');
+    window.open('/ocfemia_portfolio_react/public/Ocfemia_Resume.pdf');
   };
 
   return (
@@ -12,7 +12,7 @@ function Profile() {
         
         {/* <!-- profile picture --> */}
         <div>
-          <img src="ocfemia_portfolio_react/src/assets/img/Ocfemia_dp.jpg" className="rounded-full h-48 sm:h-56 md:h-72" alt=""/>
+          <img src="/ocfemia_portfolio_react/img/Ocfemia_dp.jpg" className="rounded-full h-48 sm:h-56 md:h-72" alt=""/>
         </div>
     
         {/* <!-- profile info --> */}
@@ -28,14 +28,14 @@ function Profile() {
               className="flex items-center gap-2 font-semibold transition-all duration-300 ease-in-out py-1 px-2 sm:py-2 sm:px-4 text-sm sm:text-base rounded-full border border-custom_gray hover:bg-custom_gray hover:text-white"
               onClick={openResume}
             >
-              <img src="/ocfemia_portfolio_react/src/assets/img/download_icon.png" className="h-4" alt=""/>
+              <img src="/ocfemia_portfolio_react/img/download_icon.png" className="h-4" alt=""/>
               Resume
             </button>
 
             {/* <!-- contact info --> */}
             <button 
               className="font-semibold transition-all duration-300 ease-in-out py-1 px-2 sm:py-2 sm:px-4 text-sm sm:text-base rounded-full border border-gray-700 bg-custom_gray text-white hover:bg-black" 
-              onClick={() => { window.location.href = './#contact'; }}>
+              onClick={() => { window.location.href = '#contact'; }}>
               Contact Info
             </button>
           </div>
@@ -43,13 +43,13 @@ function Profile() {
           {/* <!-- socials --> */}
           <div id="socials-container" className="flex gap-3">
             <img
-              src="ocfemia_portfolio_react/src/assets/img/linkedin copy.png"
+              src="/ocfemia_portfolio_react/img/linkedin copy.png"
               alt="My LinkedIn profile"
               className="h-8 cursor-pointer"
               onClick={() => window.location.href = 'https://www.linkedin.com/in/karl-brandon-ocfemia/'}
             />
             <img
-              src="ocfemia_portfolio_react/src/assets/img/github.png"
+              src="/ocfemia_portfolio_react/img/github.png"
               alt="My Github profile"
               className="h-8 cursor-pointer"
               onClick={() => { window.location.href = 'https://github.com/kb12233'}}
