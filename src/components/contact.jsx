094effc91d76005d139a2418d7adfaed1a1c37c6
kb12233 <kaybeeocfemia@gmail.com>
2024-07-16 17:13:34 +0800

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,21 +4,21 @@ function Contact() {
   return (
     <div id="contact" className="flex mb-56 items-center justify-center">
       {/* <!-- section title --> */}
-      <section className=" ">
+      <section>
         <div className="flex flex-col items-center justify-center gap-2 mb-16">
           <p className="text-sm text-custom_gray2">Get in Touch</p>
           <h1 className="font-bold text-3xl">Contact Me</h1>
         </div>
         
-        <div className="flex flex-wrap gap-2 items-center justify-center border-2 border-gray-400 rounded-full">
+        <div className="flex flex-wrap gap-2 items-center justify-center border-2 border-gray-400 mx-12 p-4 rounded-xl sm:rounded-full">
           {/* <!-- email --> */}
-          <div className="flex items-center justify-center mt-4 ml-4 mb-4">
+          <div className="flex items-center justify-center ">
             <img src="/ocfemia_portfolio_react/img/email.png" className="h-10"/>
             <a href="mailto:kbocfemia@gmail.com">kbocfemia@gmail.com</a>
           </div>
           
           {/* <!-- linkedin --> */}
-          <div className="flex items-center justify-center mt-4 mr-4 mb-4">
+          <div className="flex items-center justify-center ">
             <img src="/ocfemia_portfolio_react/img/linkedin copy.png" className="h-8"/>
             <a href="https://www.linkedin.com/in/karl-brandon-ocfemia/">LinkedIn</a>
           </div>
